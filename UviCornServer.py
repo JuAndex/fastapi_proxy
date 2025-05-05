e4fe20f239425c12edf4f0e574648288993ffe7a
JuAndex <julian.andexer@gmail.com>
2025-05-05 19:43:01 +0200

    adds logging

--- UviCornServer.py
+++ UviCornServer.py
@@ -1,10 +1,57 @@
 import threading
 import uvicorn
 import time
+import logging.config
+
+LOGGING_CONFIG = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "default": {
+            "format": "%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "default",
+            "level": "INFO",
+        },
+        "rotating_file": {
+            "class": "logging.handlers.RotatingFileHandler",
+            "filename": "uvicorn_requests.log",
+            "maxBytes": 5 * 1024 * 1024,  # 5 MB
+            "backupCount": 3,
+            "formatter": "default",
+            "level": "INFO",
+        },
+    },
+    "loggers": {
+        "uvicorn.access": {
+            "handlers": ["console", "rotating_file"],
+            "level": "INFO",
+            "propagate": False,
+        },
+        "uvicorn.error": {
+            "handlers": ["console"],
+            "level": "INFO",
+            "propagate": False,
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
+    },
+}
+
+logging.config.dictConfig(LOGGING_CONFIG)
+
 
 class UvicornServer:
     def __init__(self, app, host="127.0.0.1", port=5000):
-        self.config = uvicorn.Config(app=app, host=host, port=port, log_level="info")
+        self.config = uvicorn.Config(
+            app=app, host=host, port=port, log_level="info", log_config=LOGGING_CONFIG
+        )
         self.server = uvicorn.Server(config=self.config)
         self.thread = None
 
